ecca35b7e4e1544c2f11e12ac1876d576321fd22
hardick <bhadauriahardick@gmail.com>
2025-02-16 22:58:47 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
+  preview: {
+    port: 3000,
+  },
+});
+
